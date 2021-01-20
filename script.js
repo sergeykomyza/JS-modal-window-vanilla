3c271572085bd8df07c23fc96520f77b9acda4d1
sergeykomyza <sergeykomyza@gmail.com>
2021-01-20 15:22:02 +0300

    new-ver

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
 const modals = () => {
-  
-    const overlay = document.querySelector('.overlay');
-  
-    function bindModal(triggerSelector, modalSelector, closeSelector){
 
-        const trigger = document.querySelectorAll(triggerSelector),
-              modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
-              
-        trigger.forEach(item => {
-            item.addEventListener('click', (e) => {
-                if(e.target){
-                    e.preventDefault();
+    const overlay = document.querySelector('.overlay'); // подложка модального окна
+
+    function bindModal(triggerSelector, modalSelector) { // функция вызова мод. окна по клику
+
+        const trigger = document.querySelectorAll(triggerSelector), // кнопки вызова мод. окна
+            modal = document.querySelector(modalSelector), // селектор мод. окна
+            close = modal.querySelector('.popup-close'); // селектор закрытия мод. окна
+
+        trigger.forEach(item => { // перебираем кнопки вызова мод. окна
+            item.addEventListener('click', (e) => { // клик по одной из них
+                if (e.target) {
+                    e.preventDefault(); // отменяем действие браузера по умолчанию
                 }
-                overlay.classList.add('overlay--active');
-                modal.style.display = "block";
-                modal.classList.remove('bounceOutUp');
-                modal.classList.add('bounceInDown');
-                document.body.style.overflow = "hidden";
+                overlay.classList.add('overlay--active'); // показываем подложку
+                modal.style.display = "block"; // показываем мод. окно
+                modal.classList.remove('bounceOutUp'); // убираем класс анимации
+                modal.classList.add('bounceInDown'); // показываем класс анимации
+                document.body.style.overflow = "hidden"; // скрываем скролл окна браузера
             });
         });
-        close.addEventListener('click', () => {
-            modal.classList.remove('bounceInDown');
-            modal.classList.add('bounceOutUp');
-            overlay.classList.remove('overlay--active');
-            document.body.style.overflow = "";
+        // закрытие по клику по крестику
+        close.addEventListener('click', () => { // кликаем по крестику
+            modal.classList.remove('bounceInDown'); // убираем класс анимации
+            modal.classList.add('bounceOutUp'); // показываем класс анимации (мод. окно уезжает вверх)
+            overlay.classList.remove('overlay--active'); // скрываем подложку
+            document.body.style.overflow = ""; // возвращаем скролл окна браузера
         });
-        overlay.addEventListener('click', (e) => {
-            if(e.target === overlay){
-                modal.classList.remove('bounceInDown');
-                modal.classList.add('bounceOutUp');
-                overlay.classList.remove('overlay--active');
-                document.body.style.overflow = "";
+        // закрытие по клику вне модального окна
+        modal.addEventListener('click', (e) => { // кликаем по модальному окну 
+            if (e.target === modal) { // если клик был не по контенту модального окна
+                modal.classList.remove('bounceInDown'); // убираем класс анимации
+                overlay.classList.remove('overlay--active'); // показываем класс анимации (мод. окно уезжает вверх)
+                modal.classList.add('bounceOutUp'); // скрываем подложку
+                document.body.style.overflow = ""; // возвращаем скролл окна браузера
             }
         });
     }
-
-    function showModalByTime(selector, time){
-        setTimeout(function() {
+    // если нужно показать мод. окно при заходе на сайт
+    function showModalByTime(selector, time) {
+        setTimeout(function () {
             document.querySelector(selector).style.display = 'block';
             document.querySelector(selector).classList.add('bounceInDown');
             overlay.classList.add('overlay--active');
             document.body.style.overflow = "hidden";
         }, time);
     }
-  
-    bindModal('.popup-1-btn', '.popup-1', '.popup-1 .popup-close');
-    bindModal('.popup-2-btn', '.popup-2', '.popup-2 .popup-close');
-    bindModal('.popup-3-btn', '.popup-3', '.popup-3 .popup-close');
+
+    bindModal('.popup-1-btn', '.popup-1');
+    bindModal('.popup-2-btn', '.popup-2');
+    bindModal('.popup-3-btn', '.popup-3');
 
     showModalByTime('.popup-1', 3000);
 };
